c32e9af5fee834cee13e92d033f5fd2aa26f64fb
Vitor Mattos <vitor.mattos@vtex.com.br>
2014-10-30 03:46:28 -0200

    vim-go

--- setup-vim.sh
+++ setup-vim.sh
@@ -6,7 +6,7 @@ cd ~
 
 # Installing pathogen
 if [ ! -d $VIM_HOME/autoload ] && [ ! -d $VIM_HOME/bundle ]; then
-  echo "{blue}>>> Installing Pathogen${reset}"
+  echo "${blue}>>> Installing Pathogen"
   mkdir $VIM_HOME/autoload
   mkdir $VIM_HOME/bundle
   curl -LSso $VIM_HOME/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -16,7 +16,7 @@ fi
 
 # Installing NERDTree
 if [ ! -d $VIM_HOME/bundle/nerdtree ]; then
-  echo "${blue}>>> Installing NERDTree${reset}"
+  echo "${blue}>>> Installing NERDTree"
   git clone https://github.com/scrooloose/nerdtree.git $VIM_HOME/bundle/nerdtree
 else
   echo "${red}>>> NERDTree detected. Skipping${reset}"
@@ -24,7 +24,7 @@ fi
 
 # Installing favorite vim theme
 if [ ! -f $VIM_HOME/colors/vmattos.vim ]; then
-  echo "${blue}>>> Installing favorite vim theme${reset}"
+  echo "${blue}>>> Installing favorite vim theme"
   git clone https://gist.github.com/7ac06a16ae6a1dd4a897.git $VIM_HOME/colors/theme
   mv $VIM_HOME/colors/theme/vmattos.vim $VIM_HOME/colors
 fi
@@ -35,6 +35,14 @@ if [ ! -d $VIM_HOME/bundle/vim-coffee-script ]; then
   git clone https://github.com/kchmck/vim-coffee-script.git $VIM_HOME/bundle/vim-coffee-script/
 else
   echo "${red}>>> vim-coffee-script detected. Skipping${reset}"
+fi
+
+# Installing vim-go
+if [ ! -d $VIM_HOME/bundle/vim-go ]; then
+  echo "${blue}>>> Installing vim-go"
+   git clone https://github.com/fatih/vim-go.git $VIM_HOME/bundle/vim-go
+else
+  echo "${red}>>> vim-go detected. Skipping${reset}"
 fi
 
 if [ -f .vimrc ]; then
